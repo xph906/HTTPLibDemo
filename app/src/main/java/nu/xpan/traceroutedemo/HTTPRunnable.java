b57a467ca4c4e67d3541d2286da7bd6f46e0bd2e
Xiang Pan <xiangpan2011@gmail.com>
2016-01-20 14:28:35 -0600

    dsd

--- app/src/main/java/nu/xpan/traceroutedemo/HTTPRunnable.java
+++ app/src/main/java/nu/xpan/traceroutedemo/HTTPRunnable.java
@@ -11,8 +11,15 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 /**
  * Created by a on 1/8/16.
  */
@@ -32,19 +39,36 @@ public class HTTPRunnable implements Runnable {
 
     @Override
     public void run() {
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(url);
+        //HttpClient client = new DefaultHttpClient();
+        //HttpGet request = new HttpGet(url);
         // Making HTTP Request
         try {
+            /*
             HttpResponse response = client.execute(request);
             String result = EntityUtils.toString(response.getEntity());
             // writing response to log
+            */
+            OkHttpClient client = new OkHttpClient();
+            Request request = new okhttp3.Request.Builder()
+                    .url(this.url)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            ResponseBody body = response.body();
+            Reader charStream = body.charStream();
+            int val = -1;
+            String result = "";
+            while((val = charStream.read()) != -1){
+                result += (char)val;
+                LOGGER.info("String "+result);
+            }
+
             LOGGER.info("get response: " + result);
             Message msg = new Message();
             msg.what = MainActivity.HTTPRESPONSE_MSG;
             msg.obj = result;
             handler.sendMessage(msg);
-
+ 
         } catch (IOException e) {
             // writing exception to log
             LOGGER.severe("error: "+e.toString());
