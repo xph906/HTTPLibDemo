7a69ef11ff5dc5d2be3c5a1afe587e91a9dfacbd
xph906 <xiangpan2011@u.northwestern.edu>
2016-02-01 10:52:41 -0600

    ds

---
+++ app/src/main/java/nu/xpan/traceroutedemo/InternalConst.java
@@ -0,0 +1,11 @@
+package nu.xpan.traceroutedemo;
+
+public class InternalConst {
+    public static class MSGType {
+        public final static int TRACEROUTE_MSG = 1;
+        public final static int HTTPRESPONSE_MSG = 2;
+        public final static int ERROR_MSG = 3;
+        public final static int IMAGE_MSG = 4;
+        public final static int NETINFO_MSG = 5;
+    }
+}
